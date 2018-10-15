babc5ab71ae3558619a7a76f80fdab0a755c57fa
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2018-10-15 16:17:06 -0600

    boilerplate plated

---
+++ src/components/Colophon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Colophon = () => {
+  return (
+    <div className="colophon">
+      <h1>Colophon</h1>
+    </div>
+  )
+}
+
+export default Colophon;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import Landing from '../components/Landing';
+import About from '../components/About';
+import Experience from '../components/Experience';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import Colophon from '../components/Colophon';
+
+export default () => {
+  return (
+    <div>
+      <Landing />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Colophon />
+    </div>
+  )
+}
